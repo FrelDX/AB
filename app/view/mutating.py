6ab15078f9e7b8063550ba372fe3c6900a067ab1
曹姣月 <caojiaoyue@itcast.cn>
2019-09-29 16:02:33 +0800

    添加准入控制器

--- app/view/mutating.py
+++ app/view/mutating.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
 from flask_restful import Resource, reqparse, request
 from tools.configdb import configMap
-dbMap  =configMap
+from tools.log import logecho
+import json
+
+dbMap = configMap
+
+
 class MutatingWebhookConfiguration(Resource):
     def __init__(self):
-            super(MutatingWebhookConfiguration, self).__init__()
+        super(MutatingWebhookConfiguration, self).__init__()
     def post(self):
-        print('+_____________________________')
-        print(request.data)
-        print('+_____________________________')
-        print( request.form)
-        print('+_____________________________')
-        print('+_____________________________')
-        print('+_____________________________')
+        data = request.data
+        j_data = json.loads(data)
+        for i in j_data.keys():
+            logecho.info(i)
+        logecho.info(j_data["request"]["uid"])
+        logecho.info(j_data["request"]["object"])
+
