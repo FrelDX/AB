e1c2490e732acdedcd1673964e798efc9896a546
曹姣月 <caojiaoyue@itcast.cn>
2019-10-07 11:05:56 +0800

    修复容器重复注入

--- test/yaml/svc.py
+++ test/yaml/svc.py
@@ -4,6 +4,7 @@ import jsonpatch
 import base64
 import json
 import struct
+
 path = '/spec/template/spec'
 pathc1 = '[{"op": "add", "path": "/spec/replicas", "value": 3}]'
 pathc2 = [{"op": "add", "path": "/spec/replicas", "value": 3}]
@@ -13,9 +14,16 @@ print(pathc1 == pathc2)
 # print(base64.b64encode(pathc2))
 print(base64.b64encode(pathc1.encode('utf8')))
 srt = base64.b64encode(pathc2.encode('utf8'))
-srt = str(srt,encoding='utf8')
+srt = str(srt, encoding='utf8')
 print(srt)
 
-
-
-
+s = [{'name': 'test', 'image': 'tomcat', 'resources': {}, 'terminationMessagePath': '/dev/termination-log',
+      'terminationMessagePolicy': 'File', 'imagePullPolicy': 'Always'},
+     {'name': 'nginx', 'image': 'nginx', 'resources': {}, 'terminationMessagePath': '/dev/termination-log',
+      'terminationMessagePolicy': 'File', 'imagePullPolicy': 'Always'}]
+s = [{'name': 'test', 'image': 'tomcat', 'resources': {}, 'terminationMessagePath': '/dev/termination-log',
+      'terminationMessagePolicy': 'File', 'imagePullPolicy': 'Always'},
+     {'name': 'nginx', 'image': 'nginx', 'resources': {}, 'terminationMessagePath': '/dev/termination-log',
+      'terminationMessagePolicy': 'File', 'imagePullPolicy': 'Always'},
+     {'name': 'nginx', 'image': 'nginx', 'resources': {}, 'terminationMessagePath': '/dev/termination-log',
+      'terminationMessagePolicy': 'File', 'imagePullPolicy': 'Always'}]
