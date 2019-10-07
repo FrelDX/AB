2fb7e92a69e5e9d801bcbc9ea4f46a2e6d4769ed
曹姣月 <caojiaoyue@itcast.cn>
2019-10-07 11:54:01 +0800

    更改代码结构

--- app/view/mutating.py
+++ app/view/mutating.py
@@ -13,37 +13,57 @@ class MutatingWebhookConfiguration(Resource):
         super(MutatingWebhookConfiguration, self).__init__()
     def post(self):
         j_data = request.get_json()
-        logecho.info(j_data)
-        for i in j_data.keys():
-            logecho.info(i)
-        logecho.info(j_data["request"]["uid"])
-        logecho.info(j_data["request"]["object"])
-
-        into = j_data["request"]["object"]["spec"]["template"]["spec"]["containers"]
-        logecho.info("-----------------into-------------")
-        logecho.info(into)
-        logecho.info("-----------------into-------------")
-        intobody = {'name': 'nginx', 'image': 'nginx', 'imagePullPolicy': 'Always'}
-        into .append(intobody)
-        logecho.info("-----------------into add -------------")
-        logecho.info(into)
-        logecho.info("-----------------into add -------------")
-        jsonpath = [
-            {"op": "replace", "path": "/spec/template/spec/containers", "value": into}
-        ]
-        pathc2 = json.dumps(jsonpath)
-        srt = base64.b64encode(pathc2.encode('utf8'))
-        srt = str(srt, encoding='utf8')
-        logecho.info(srt)
-        s = {
+        process = pipline(j_data)
+        body = {
         "apiVersion": "admission.k8s.io/v1beta1",
         "kind": "AdmissionReview",
         "response": {
         "uid": j_data['request']['uid'],
         "allowed": True,
         "patchType": "JSONPatch",
-        "patch": srt
+        "patch": process.toInto()
          }}
-        return s
+        return body
+
+
+
+class pipline():
+    def __init__(self,body:json):
+        self.body = body
+    def getInto(self):
+        """
+        :return:  获取注入的body
+        """
+        return {'name': 'nginx', 'image': 'nginx:1.12.2', 'imagePullPolicy': 'Always'}
+    def filtration(self):
+        """
+        :return:  根据注入条件匹配注入的body
+        """
+    def toInto(self):
+        """
+        :return: 注入
+        """
+        ###用户自定义的containers
+        sourceBody = self.body["request"]["object"]["spec"]["template"]["spec"]["containers"]
+        ###需要注入的containers
+        intoBody = self.getInto()
+        #最终注入体
+        newInto = sourceBody.append(intoBody)
+        jsonpath = [
+            {"op": "replace", "path": "/spec/template/spec/containers", "value": newInto}
+        ]
+        jsonpath = json.dumps(jsonpath)
+        body = base64.b64encode(jsonpath.encode('utf8'))
+        body = str(body, encoding='utf8')
+        return body
+
+
+
+
+
+
+
+
+
 
 
