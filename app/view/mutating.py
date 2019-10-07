9f4e6bd8570c1b364eae49d5e5f2edadc72fe86d
曹姣月 <caojiaoyue@itcast.cn>
2019-10-07 10:30:55 +0800

    .

--- app/view/mutating.py
+++ app/view/mutating.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, reqparse, request
 from tools.configdb import configMap
 from tools.log import logecho
 import json
+import base64
 
 dbMap = configMap
 
@@ -11,20 +12,34 @@ class MutatingWebhookConfiguration(Resource):
     def __init__(self):
         super(MutatingWebhookConfiguration, self).__init__()
     def post(self):
-        data = request.data
         j_data = request.get_json()
         logecho.info(j_data)
         for i in j_data.keys():
             logecho.info(i)
         logecho.info(j_data["request"]["uid"])
         logecho.info(j_data["request"]["object"])
-        data = request.get_json()
+
+        into = j_data["request"]["object"]["spec"]["template"]["spec"]["containers"]
+        intobody = {'name': 'nginx', 'image': 'nginx', 'resources': {}, 'terminationMessagePath': '/dev/termination-log',
+          'terminationMessagePolicy': 'File', 'imagePullPolicy': 'Always'}
+        into .append(intobody)
+        logecho.info(into)
+        jsonpath = [
+            {"op": "replace", "path": "/spec/template/spec/containers", "value": into}
+        ]
+        pathc2 = json.dumps(jsonpath)
+        srt = base64.b64encode(pathc2.encode('utf8'))
+        srt = str(srt, encoding='utf8')
+        logecho.info(srt)
         s = {
         "apiVersion": "admission.k8s.io/v1beta1",
         "kind": "AdmissionReview",
         "response": {
-        "uid": data['request']['uid'],
+        "uid": j_data['request']['uid'],
         "allowed": True,
         "patchType": "JSONPatch",
-        "patch": "W3sib3AiOiAiYWRkIiwgInBhdGgiOiAiL3NwZWMvcmVwbGljYXMiLCAidmFsdWUiOiAzfV0="
+        "patch": srt
          }}
+        return s
+
+
--- test/yaml/svc.py
+++ test/yaml/svc.py
@@ -1,46 +1,21 @@
 # -*- coding: utf-8 -*-
 import jsonpatch
-doc = { 'apiVersion': 'apps/v1',
-  'kind': 'Deployment',
-  'metadata': { 'name': 'centos', 'labels': { 'app': 'centos' },'namespace':'test' },
-  'spec':
-   { 'replicas': 1,
-     'template':
-      { 'metadata': { 'name': 'centos', 'labels': { 'app': 'centos' } },
-        'spec':
-         { 'nodeSelector': { 'gogs': 'true' },
-           'containers':
-            [ { 'name': 'centos',
-                'image': 'centos',
-                'command': [ '/bin/bash' ],
-                'ports': [ { 'containerPort': '9200' } ],
-                'env':
-                 [ { 'name': 'ES_JAVA_OPTS', 'value': '-Xms2048m -Xmx2048m' },
-                   { 'name': 'discovery.type', 'value': 'single-node' } ] } ] } },
-     'selector': { 'matchLabels': { 'app': 'centos' } } } }
 
-body  = {"containers":
-   [{"name": 'ab',
-       "image": 'registry.cn-hangzhou.aliyuncs.com/caojiaoyue/ab:test7',
-       "imagePullPolicy": 'IfNotPresent',
-       "ports": [ { "containerPort": "5000" } ] } ],
-  "serviceAccountName": {'ab':234},
-  "restartPolicy": 'Always' }
+import base64
+import json
+import struct
 path = '/spec/template/spec'
-pathc = [{'op': 'add', 'path': '/spec/template/spec/containers', 'value': body},{'op': 'add', 'path': '/spec/template/spec/containers', 'value': body}]
+pathc1 = '[{"op": "add", "path": "/spec/replicas", "value": 3}]'
+pathc2 = [{"op": "add", "path": "/spec/replicas", "value": 3}]
+pathc2 = json.dumps(pathc2)
+print(pathc1 == pathc2)
+# print(base64.b64encode(pathc1))
+# print(base64.b64encode(pathc2))
+print(base64.b64encode(pathc1.encode('utf8')))
+srt = base64.b64encode(pathc2.encode('utf8'))
+srt = str(srt,encoding='utf8')
+print(srt)
 
 
-a = jsonpatch.JsonPatch(pathc)
-
-result = a.apply(doc)
-print(result)
 
 
-s = """
-
-{'apiVersion': 'apps/v1', 'kind': 'Deployment', 'metadata': {'name': 'centos', 'labels': {'app': 'centos'}, 'namespace': 'test'}, 'spec': {'replicas': 1, 'template': {'metadata': {'name': 'centos', 'labels': {'app': 'centos'}}, 'spec': {'containers': [{'name': 'ab', 'image': 'registry.cn-hangzhou.aliyuncs.com/caojiaoyue/ab:test7', 'imagePullPolicy': 'IfNotPresent', 'ports': [{'containerPort': '5000'}]}], 'serviceAccountName': {'ab': 234}, 'restartPolicy': 'Always'}}, 'selector': {'matchLabels': {'app': 'centos'}}}}
-
-"""
-
-
-print(s.replace("'","\""))
